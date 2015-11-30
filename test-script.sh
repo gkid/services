61a32a128c4f97808a29eeab4d40d414aae62ae3
gkid <github.com@mailnicks.org>
2015-11-30 15:48:41 +0200

    removed tar & s3 cp

--- test-script.sh
+++ test-script.sh
@@ -6,10 +6,7 @@ ls -la >> test.txt
 echo "\nnice test\n==========================================================================" >> test.txt
 aws s3api list-buckets --query 'Buckets[].Name' >> test.txt
 
-tar -czf test.txt.tar.gz test.txt
-
 ls
 
 echo "----------------------------"
-aws s3 cp test.txt.tar.gz s3://aaaa-test/
 echo "done"
