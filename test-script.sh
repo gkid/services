bc4fe2ee47d722164174c499b789aa9b20664902
gkid <github.com@mailnicks.org>
2015-11-30 14:56:28 +0200

    Update test-script.sh

--- test-script.sh
+++ test-script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "=========================================================================="
-ls -la
+echo "==========================================================================" > test.txt
+ls -la >> test.txt
 
-echo "\nnice test\n=========================================================================="
-aws s3api list-buckets --query 'Buckets[].Name'
+echo "\nnice test\n==========================================================================" >> test.txt
+aws s3api list-buckets --query 'Buckets[].Name' >> test.txt
+
+tar -czf test.txt.tar.gz test.txt
