f12ec42151b1646ea4b4852ef0b27fa3e24a89cb
gkid <github.com@mailnicks.org>
2015-12-15 13:02:01 +0200

    Update evenfail.sh

--- evenfail.sh
+++ evenfail.sh
@@ -2,10 +2,10 @@
 
 if [ $((GO_STAGE_COUNTER % 2)) -eq 0 ]
 then
-   echo "$GO_STAGE_COUNTER is even - failing"
-   exit 1 
+   echo "$GO_STAGE_COUNTER is even"
 else
-   echo "$GO_STAGE_COUNTER is odd"
+   echo "$GO_STAGE_COUNTER is odd - failing"
+   exit 1 
 fi
 
 echo "----------------------------"
