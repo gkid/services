4b583d7e3d67ea7bf6bb131c798766408ca8a582
gkid <github.com@mailnicks.org>
2015-12-15 12:23:29 +0200

    Update done.sh

--- done.sh
+++ done.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "----------------------------"
-echo "done.sh is DONE"
+echo "done.sh - pipe: $GO_PIPELINE_NAME / $GO_PIPELINE_COUNTER stage#: $GO_STAGE_NAME / $GO_STAGE_COUNTER job: $GO_JOB_NAME is DONE"
